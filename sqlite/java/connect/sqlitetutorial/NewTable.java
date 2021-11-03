42b84e841ff913ca6c9090ba8ff00c33cf7b4743
veerendra08 <58736990+veerendra08@users.noreply.github.com>
2021-11-03 19:40:41 +0530

    Update NewTable.java

--- sqlite/java/connect/sqlitetutorial/NewTable.java
+++ sqlite/java/connect/sqlitetutorial/NewTable.java
@@ -5,21 +5,15 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-/**
- *
- * @author sqlitetutorial.net
- */
+
 public class NewTable {
 
-    /**
-     * Create a new table in the test database
-     *
-     */
+   
     public static void createNewTable() {
-        // SQLite connection string
+      
         String url = "jdbc:sqlite:C://sqlite/db/Movies.db";
         
-        // SQL statement for creating a new table
+      
          String sql = "CREATE TABLE IF NOT EXISTS Movies (\n"
                 + "	Name text PRIMARY KEY,\n"
                 + "	Actor_Name text NOT NULL,\n"
@@ -30,16 +24,14 @@ public class NewTable {
         
         try (Connection conn = DriverManager.getConnection(url);
                 Statement stmt = conn.createStatement()) {
-            // create a new table
+         
             stmt.execute(sql);
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
     }
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         createNewTable();
     }
