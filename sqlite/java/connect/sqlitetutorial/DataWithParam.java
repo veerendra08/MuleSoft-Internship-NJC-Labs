a9b9a80cd0864c402e95b2358ea03a9bc5971948
veerendra08 <58736990+veerendra08@users.noreply.github.com>
2021-11-03 19:39:09 +0530

    Update DataWithParam.java

--- sqlite/java/connect/sqlitetutorial/DataWithParam.java
+++ sqlite/java/connect/sqlitetutorial/DataWithParam.java
@@ -8,14 +8,10 @@ import java.sql.Statement;
 import java.sql.PreparedStatement;
 import java.util.*;
 
-/**
- *
- * @author sqlitetutorial.net
- */
 public class DataWithParam {
 
  private Connection connect() {
-        // SQLite connection string
+       
         String url = "jdbc:sqlite:C://sqlite/db/Movies.db";
         Connection conn = null;
         try {
@@ -25,21 +21,18 @@ public class DataWithParam {
         }
         return conn;
     }
-        /**
-     * Get the warehouse whose capacity greater than a specified capacity
-     * @param capacity 
-     */
+        
     public void getMovieWithName(String name){
                String sql = "SELECT Name,Actor_Name,Actress_Name,Year_of_Release,Director FROM Movies WHERE Actor_Name = ?";
         	 
         try (Connection conn = this.connect();PreparedStatement pstmt  = conn.prepareStatement(sql)){
             
-            // set the value
+           
             pstmt.setString(1,name);
-            //
+            
             ResultSet rs  = pstmt.executeQuery();
             
-            // loop through the result set
+           
             while (rs.next()) {
                 System.out.println(rs.getString("Name") +  "\t" + 
                                    rs.getString("Actor_Name") + "\t" +
@@ -53,9 +46,7 @@ public class DataWithParam {
     }
     
    
-    /**
-     * @param args the command line arguments
-     */
+  
     public static void main(String[] args) {
         DataWithParam app = new DataWithParam();
 	Scanner sc = new Scanner(System.in);
