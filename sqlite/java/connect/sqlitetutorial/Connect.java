1d00f37a0bc1c8612407d601277eb3ea968c03c4
veerendra08 <58736990+veerendra08@users.noreply.github.com>
2021-11-03 19:38:14 +0530

    Update Connect.java

--- sqlite/java/connect/sqlitetutorial/Connect.java
+++ sqlite/java/connect/sqlitetutorial/Connect.java
@@ -4,20 +4,15 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-/**
- *
- * @author sqlitetutorial.net
- */
+
 public class Connect {
-     /**
-     * Connect to a sample database
-     */
+    
     public static void connect() {
         Connection conn = null;
         try {
-            // db parameters
+           
             String url = "jdbc:sqlite:C:/sqlite/db/Movies.db";
-            // create a connection to the database
+           
             conn = DriverManager.getConnection(url);
             
             System.out.println("Connection to SQLite has been established.");
@@ -34,9 +29,7 @@ public class Connect {
             }
         }
     }
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
         connect();
     }
