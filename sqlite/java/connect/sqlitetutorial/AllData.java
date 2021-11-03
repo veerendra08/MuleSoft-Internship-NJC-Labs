ccb7dfed6b11963038ef428098c7a30c566c828c
veerendra08 <58736990+veerendra08@users.noreply.github.com>
2021-11-03 19:37:16 +0530

    Update AllData.java

--- sqlite/java/connect/sqlitetutorial/AllData.java
+++ sqlite/java/connect/sqlitetutorial/AllData.java
@@ -6,18 +6,11 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-/**
- *
- * @author sqlitetutorial.net
- */
 public class AllData {
 
-    /**
-     * Connect to the test.db database
-     * @return the Connection object
-     */
+   
     private Connection connect() {
-        // SQLite connection string
+      
         String url = "jdbc:sqlite:C://sqlite/db/Movies.db";
         Connection conn = null;
         try {
@@ -29,9 +22,7 @@ public class AllData {
     }
 
     
-    /**
-     * select all rows in the warehouses table
-     */
+   
     public void selectAll(){
         String sql = "SELECT Name,Actor_Name,Actress_Name,Year_of_Release,Director FROM Movies";
         
@@ -39,7 +30,6 @@ public class AllData {
              Statement stmt  = conn.createStatement();
              ResultSet rs    = stmt.executeQuery(sql)){
             
-            // loop through the result set
             while (rs.next()) {
                 System.out.println(rs.getString("Name") +  "\t" + 
                                    rs.getString("Actor_Name") + "\t" +
@@ -53,9 +43,7 @@ public class AllData {
     }
     
    
-    /**
-     * @param args the command line arguments
-     */
+  
     public static void main(String[] args) {
         AllData app = new AllData();
         app.selectAll();
